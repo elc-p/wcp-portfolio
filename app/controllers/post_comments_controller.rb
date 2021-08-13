d2de0db1c447fb115586e31910f55de7dc2d72f4
elc-p <yomtype10@gmail.com>
2021-08-13 11:05:23 +0000

    [Update]favorite-ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,14 +6,14 @@ class PostCommentsController < ApplicationController
     @post_comment.post_id = @post.id
     @post_comment.user_id = current_user.id
     @post_comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   def destroy
     @post = Post.find(params[:post_id])
     post_comment = @post.post_comments.find(params[:id])
     post_comment.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   
