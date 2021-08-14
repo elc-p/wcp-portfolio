a3dfbe0e3ec890b6dbc7e5fd6741ada74e65f47a
elc-p <yomtype10@gmail.com>
2021-08-14 05:41:22 +0000

    [Update]follow-ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,17 @@
 class RelationshipsController < ApplicationController
   
   def create
+    @user = current_user
+    @another_user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   def destroy
+    @user = current_user
+    @another_user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   def followings
