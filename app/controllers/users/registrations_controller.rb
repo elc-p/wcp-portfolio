bb6369900fad774718e2a3efdd69425eaabce6ca
elc-p <yomtype10@gmail.com>
2021-08-25 22:51:02 +0000

    []

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  
+  def create
+    super
+    UserMailer.welcome_email(params[:user][:email], params[:user][:name]).deliver
+  end
 
   # GET /resource/sign_up
   # def new
@@ -59,4 +64,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  
 end
