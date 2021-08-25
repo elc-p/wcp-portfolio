27bde2c9545c4d33acfe0a962eab122de603c71d
elc-p <yomtype10@gmail.com>
2021-08-25 22:49:41 +0000

    [Complete]Actionmailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'とりちゃんお便り'
+  default from: 'とりちゃん事務局 <torichan.test@gmail.com>'
   layout 'mailer'
   
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email, name)
     @user = name
+    # byebug
     mail(to: email, subject: 'とりちゃんへようこそ')
   end
 
