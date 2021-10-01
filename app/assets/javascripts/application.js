08f72561a00163d73806a7d951d92bd642babbf8
elc-p <yomtype10@gmail.com>
2021-10-01 01:50:07 +0000

    [Fix]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require jquery
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery_ujs
 //= require_tree .
 
 
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,10 +3,11 @@ class PostCommentsController < ApplicationController
   
   def create
     @post = Post.find(params[:post_id])
-    @post_comment = PostComment.new(comment_params)
-    @post_comment.post_id = @post.id
-    @post_comment.user_id = current_user.id
-    @post_comment.save
+    post_comment = PostComment.new(comment_params)
+    post_comment.post_id = @post.id
+    post_comment.user_id = current_user.id
+    post_comment.save
+    @post_comment = PostComment.new
     # redirect_to request.referer
   end
   
